177b799258ba9699430422fb55014e2f55a2d53b
DigoRM <82246925+DigoRM@users.noreply.github.com>
2021-10-04 21:16:42 -0300

    Update Analyze_Appcall.py

--- Analyze_Appcall.py
+++ Analyze_Appcall.py
@@ -44,29 +44,13 @@ input_prechurn = st.number_input('Digite o parâmetro a ser utilizado como pre-C
 input_Estorno = st.number_input('Digite o percentual de estorno aceitável',min_value=0.0,max_value=100.0,value=5.0,step=0.25)
 input_Chargeback = st.number_input('Digite o percentual de chargeback aceitável',min_value=0.0,max_value=100.0,value=2.0,step=0.25)
 
-# Churn
-df ['Dimensionamento_AppCall'] = (df['Total Call Center']/df['Total Aprovado'])*100
-df.loc[(df['Total Aprovado'] == 0), 'Status_Processamento'] = "CHURN"
-df.loc[(df['Total Aprovado'] > 0)&(df['Total Aprovado'] <= input_prechurn), 'Status_Processamento'] = "Pre-CHURN"
-df.loc[(df['Total Aprovado'] > input_prechurn), 'Status_Processamento'] = "Processando"
-
-# Estorno
-df ['Percentual_Estornos'] = (df['Pedidos Estornados']/df['Total Aprovado'])*100
-df.loc[(df['Percentual_Estornos'] > input_Estorno), 'Status_Estorno'] = "Alerta"
-df.loc[(df['Percentual_Estornos'] <= input_Estorno), 'Status_Estorno'] = "aceitável"
-df.loc[(df['Percentual_Estornos'] > 50.0), 'Status_Estorno'] = "PREOCUPANTE"
-
-# Charageback
-df ['Percentual_Chargeback'] = (df['Pedidos Chargeback']/df['Total Aprovado'])*100
-df.loc[(df['Percentual_Chargeback'] > input_Chargeback), 'Status_Chargeback'] = "Alerta"
-df.loc[(df['Percentual_Chargeback'] <= input_Chargeback), 'Status_Chargeback'] = "aceitável"
-df.loc[(df['Percentual_Chargeback'] > 20.0), 'Status_Chargeback'] = "PREOCUPANTE"
-
 
 df = df.rename(columns = {'Total Aprovado': 'Total_Aprovado'}, inplace = False)
 
 ranking_appcall= df
 # Segmentação por Contribuição do CallCenter no Faturamento do site
+
+ranking_appcall ['Dimensionamento_AppCall'] = ranking_appcall ['Total Call Center']/ranking_appcall ['Total_Aprovado']
 ranking_appcall ['Dimensionamento_AppCall'].fillna(0, inplace=True)
 
 ranking_appcall ['Status_Appcall'] = ranking_appcall  ['Dimensionamento_AppCall']
@@ -81,17 +65,36 @@ Estatisticas_Status_Appcall = ranking_appcall_QntParceiro.groupby('Status_Appcal
 Estatisticas_Status_Appcall = Estatisticas_Status_Appcall.drop(columns=['Dimensionamento_AppCall'])
 Estatisticas_Status_Appcall ['Média Leads'] = Estatisticas_Status_Appcall['Leads']/Estatisticas_Status_Appcall['Quantidade Sites']
 
+# Churn
+ranking_appcall.loc[(df['Total_Aprovado'] == 0), 'Status_Processamento'] = "CHURN"
+ranking_appcall.loc[(df['Total_Aprovado'] > 0)&(df['Total_Aprovado'] <= input_prechurn), 'Status_Processamento'] = "Pre-CHURN"
+ranking_appcall.loc[(df['Total_Aprovado'] > input_prechurn), 'Status_Processamento'] = "Processando"
+
+# Estorno
+ranking_appcall ['Percentual_Estornos'] = (ranking_appcall['Pedidos Estornados']/ranking_appcall['Total_Aprovado'])*100
+ranking_appcall.loc[(df['Percentual_Estornos'] > input_Estorno), 'Status_Estorno'] = "Alerta"
+ranking_appcall.loc[(df['Percentual_Estornos'] <= input_Estorno), 'Status_Estorno'] = "aceitável"
+ranking_appcall.loc[(df['Percentual_Estornos'] > 50.0), 'Status_Estorno'] = "PREOCUPANTE"
+
+# Charageback
+ranking_appcall ['Percentual_Chargeback'] = (ranking_appcall['Pedidos Chargeback']/ranking_appcall['Total_Aprovado'])*100
+ranking_appcall.loc[(df['Percentual_Chargeback'] > input_Chargeback), 'Status_Chargeback'] = "Alerta"
+ranking_appcall.loc[(df['Percentual_Chargeback'] <= input_Chargeback), 'Status_Chargeback'] = "aceitável"
+ranking_appcall.loc[(df['Percentual_Chargeback'] > 20.0), 'Status_Chargeback'] = "PREOCUPANTE"
+
+ranking_appcall
+
 # Filter in SideBar
 status_processamento =  st.sidebar.multiselect('Selecione o Status da Operação',options=ranking_appcall['Status_Processamento'].unique(),default=ranking_appcall['Status_Processamento'].unique())
 squad = st.sidebar.multiselect ("Selecione o Squad", options=ranking_appcall['Squad'].unique(),default=ranking_appcall['Squad'].unique())
 Ativo_Inativo = st.sidebar.multiselect ("Segmentação Appcall", options=ranking_appcall['Status_Appcall'].unique(),default=ranking_appcall['Status_Appcall'].unique())
-# Situation_Reversals = st.sidebar.multiselect ("ESTORNO", options=ranking_appcall['Status_Estorno'].unique(),default=ranking_appcall['Status_Estorno'].unique())
-# Situation_Chargeback = st.sidebar.multiselect ("CHARGEBACK", options=ranking_appcall['Status_Chargeback'].unique(),default=ranking_appcall['Status_Chargeback'].unique())
+situation_Reversals = st.sidebar.multiselect ("ESTORNO", options=ranking_appcall['Status_Estorno'].unique(),default=ranking_appcall['Status_Estorno'].unique())
+situation_Chargeback = st.sidebar.multiselect ("CHARGEBACK", options=ranking_appcall['Status_Chargeback'].unique(),default=ranking_appcall['Status_Chargeback'].unique())
 
 
 # Resultado da Query
 # st.header('Resultado do Filtro')
-df_selection_AppCall = ranking_appcall.query("Status_Processamento == @status_processamento & Status_Appcall == @Ativo_Inativo & Squad == @squad" )
+df_selection_AppCall = ranking_appcall.query("Status_Processamento == @status_processamento & Status_Appcall == @Ativo_Inativo & Squad == @squad " )
 st.header('Ranking AppCall')
 df_selection_AppCall = df_selection_AppCall.sort_values('Total Call Center',ascending=False)
 st.dataframe(df_selection_AppCall)
@@ -170,16 +173,9 @@ with c4:
 	st.plotly_chart(Grafico_df_selection_Leads,use_container_width=True)
 
 
-situation_Reversals = st.sidebar.multiselect ("ESTORNO", options=ranking_appcall['Status_Estorno'].unique(),default=ranking_appcall['Status_Estorno'].unique())
-situation_Chargeback = st.sidebar.multiselect ("CHARGEBACK", options=ranking_appcall['Status_Chargeback'].unique(),default=ranking_appcall['Status_Chargeback'].unique())
-
-
-
-df_selection_revolution = ranking_appcall.query("Squad == @squad & Status_Estorno == @situation_Reversals & Status_Chargeback == @situation_Chargeback" )
-
-df_selection_revolution 
-
-
+df_selection_Evolution = ranking_appcall.query("Status_Estorno == @situation_Reversals & Status_Chargeback == @situation_Chargeback" )
+df_selection_Evolution
+	
 
 
 
